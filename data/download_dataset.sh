0891a9a66aa66d2146b8d43d50b4912cbc2ddae4
SilkenMocha_Alpha <ericklopezsaldivar@gmail.com>
2025-07-16 13:58:56 -0600

    Descarga de Dataset OE62

--- data/download_dataset.sh
+++ data/download_dataset.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-wget http://deepchem.io.s3-website-us-west-1.amazonaws.com/datasets/gdb9.tar.gz 
-tar xvzf gdb9.tar.gz
-rm gdb9.tar.gz
+wget -O OE62_dataset.zip https://dataserv.ub.tum.de/index.php/s/m1507656/download
+unzip OE62_dataset.zip
+rm OE62_dataset.zip
 
 wget https://github.com/gablg1/ORGAN/raw/master/organ/NP_score.pkl.gz
 wget https://github.com/gablg1/ORGAN/raw/master/organ/SA_score.pkl.gz
+
+
